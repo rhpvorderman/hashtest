6c12a31894ff3da29e49a6815997a4e561395f5a
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2020-03-13 10:59:16 +0100

    finish benchmarking code

--- src/test/scala/net/biowdl/hashtesttest/HashAlgorithmsBenchmark.scala
+++ src/test/scala/net/biowdl/hashtesttest/HashAlgorithmsBenchmark.scala
@@ -1,27 +1,35 @@
 package net.biowdl.hashtesttest
 
 import java.io.InputStream
-import java.util
 import java.util.Calendar
 
 import better.files.{File, Resource}
 import net.biowdl.hashtest.HashAlgorithms
-import org.scalatest.{Assertion, Matchers}
-import org.scalatest.testng.TestNGSuite
 import org.testng.annotations.{DataProvider, Test}
 
+import scala.collection.mutable.ListBuffer
+
 class HashAlgorithmsBenchmark {
-  val randombase64File: File = File(Resource.getUrl("randombase64"))
+  val bigBwaIndexFile: File = File(Resource.getUrl("BigBwaIndex.tar.gz"))
+  val md5: InputStream => String = HashAlgorithms.md5
+  val xxh64: InputStream => String = HashAlgorithms.xxh64lz4(_)
+  val xxh32: InputStream => String = HashAlgorithms.xxh32lz4(_)
 
+  @DataProvider
+  def benchmarks(): Array[Array[Any]] = Array(
+    Array(xxh64, bigBwaIndexFile, "xxh64sum", 3, 10),
+    Array(xxh32, bigBwaIndexFile, "xxh32sum", 3, 10),
+    Array(md5, bigBwaIndexFile, "md5sum", 3 ,10),
+  )
 
-  def benchmark(hashFunction: InputStream => String, file: File): Float = {
+  def benchmark(hashFunction: InputStream => String, file: File): Long = {
     val start: Long = Calendar.getInstance().getTimeInMillis
     hashFunction(file.newInputStream)
     val end: Long = Calendar.getInstance().getTimeInMillis
-    val seconds: Float = (end - start ) / 1000
-    seconds
+    end - start
   }
 
+  @Test(dataProvider = "benchmarks")
   def hyperfine(hashFunction: InputStream => String,
                 file: File,
                 testName: String,
@@ -31,13 +39,12 @@ class HashAlgorithmsBenchmark {
     for (_ <- 1 to warmup) {
       hashFunction(file.newInputStream)
     }
-    val results = Array[Float](runs)
+    val results: ListBuffer[Float] = ListBuffer()
     println(s"Perform $runs test runs for $testName.")
     for (i <- 0 until runs) {
       val result = benchmark(hashFunction, file)
-      results(i) = result
-      println(s"Run $i: $result seconds)}")
+      results.append(result)
     }
-    val min = results.foldLeft()
+    print(s"${testName} results. Average=${results.sum / runs}ms, min=${results.min}ms, max=${results.max}ms")
   }
 }
