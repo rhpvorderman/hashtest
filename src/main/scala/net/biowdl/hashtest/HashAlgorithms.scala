17082be445c2dc83f4b33248e259f2830c03a1a7
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2020-03-13 08:14:43 +0100

    add some hashing algorithms

--- src/main/scala/net/biowdl/hashtest/HashAlgorithms.scala
+++ src/main/scala/net/biowdl/hashtest/HashAlgorithms.scala
@@ -1,5 +1,32 @@
 package net.biowdl.hashtest
 
+import net.openhft.hashing.LongHashFunction
+import net.jpountz.xxhash.StreamingXXHash64
+import net.jpountz.xxhash.StreamingXXHash32
+import org.apache.commons.codec.digest.DigestUtils
+import java.io.BufferedInputStream
 object HashAlgorithms {
+  lazy val defaultBufferSize = 32 * 1024
+  def md5(inputStream: BufferedInputStream): String = {
+    DigestUtils.md5Hex(inputStream)
+  }
 
+  def xxh64OpenHft(inputStream: BufferedInputStream,
+                   bufferSize: Int = defaultBufferSize
+                  ): String = {
+    val buffer: Array[Byte] = new Array[Byte](bufferSize)
+    var xxh64sum: Long = 0L
+    while (inputStream.available() > 0) {
+      inputStream.read(buffer)
+      xxh64sum = LongHashFunction.xx(xxh64sum).hashBytes(buffer)
+    }
+    xxh64sum.toHexString
+  }
+
+  def xxh64lz4(inputStream: BufferedInputStream,
+               buffersize: Int = defaultBufferSize): String = {
+    val hasher = StreamingXXHash64
+
+  }
 }
+
